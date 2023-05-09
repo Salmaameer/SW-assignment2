298c1e0f0dfb025cdfcacab5bbb8657f21355adb
shahdkh2288 <101909725+shahdkh2288@users.noreply.github.com>
2023-05-09 04:07:42 +0300

    Create catalog.java

---
+++ catalog.java
@@ -0,0 +1,24 @@
+package toffee;
+
+import java.util.ArrayList;
+import java.util.List;
+public class catalog {
+    private List<Category> categories;
+    public catalog() {
+        categories = new ArrayList<>();
+    }
+    public void setCategories(List<Category> categories) {
+        this.categories = categories;
+    }
+    public List<Category> getCategories() {
+        return categories;
+    }
+    public void addCategory(Category category) {
+        categories.add(category);
+    }
+    public void showItems() {
+        for (Category category : categories) {
+            category.showItem();
+        }
+    }
+}
