a84f5b382cbb47ab025077efc4a95e5a00c46716
shahdkh2288 <101909725+shahdkh2288@users.noreply.github.com>
2023-05-09 04:07:47 +0300

    Create Category.java

---
+++ Category.java
@@ -0,0 +1,46 @@
+package toffee;
+
+public class Category {
+    private String name;
+    private int pQuantity;
+    private Product[] products;
+    private double discountP;
+    public Category(String name, int pQuantity, Product[] products, double discountP) {
+        this.name = name;
+        this.pQuantity = pQuantity;
+        this.products = products;
+        this.discountP = discountP;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setPQuantity(int pQuantity) {
+        this.pQuantity = pQuantity;
+    }
+    public void setProducts(Product[] products) {
+        this.products = products;
+    }
+    public void setDiscountP(double discountP) {
+        this.discountP = discountP;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getPQuantity() {
+        return pQuantity;
+    }
+    public Product[] getProducts() {
+        return products;
+    }
+    public double getDiscountP() {
+        return discountP;
+    }
+
+        public void showItem() {
+        System.out.println("Category Name: " + name);
+        System.out.println("Num Of Products: " + pQuantity);
+        System.out.println("Discount Percentage: " + discountP);
+    }
+
+
+}
